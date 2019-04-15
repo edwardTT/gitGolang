2f867c6f28d01789ce2302bdef7c3e99d04d9dba
edwardTT <bwxzing@163.com>
2019-04-15 17:43:26 +0800

    优化代码 加入Reader Writer 接口。代码更加易读

--- goroutine/readwriteTest/rwgoroutine.go
+++ goroutine/readwriteTest/rwgoroutine.go
@@ -6,41 +6,62 @@ import (
 	"time"
 )
 
+type Reader interface {
+	Read(rc chan string)
+}
+
+type Writer interface {
+	Write(wc chan string)
+}
 type LogProcess struct {
-	rc          chan string
-	wc          chan string
-	path        string
+	rc    chan string
+	wc    chan string
+	read  Reader
+	write Writer
+}
+type ReadFromFile struct {
+	path string
+}
+
+func (r *ReadFromFile) Read(rc chan string) {
+	// 读取模块
+	line := "message"
+	rc <- line
+}
+
+type WriteToInfluxDB struct {
 	influxBDDsn string
 }
 
-func (l *LogProcess) ReadFromFile() {
-	// 读取模块
-	line := "message"
-	l.rc <- line
-}
+func (w *WriteToInfluxDB) Write(wc chan string) {
+	//写入模块
+	fmt.Println(<-wc)
 
+}
 func (l *LogProcess) Process() {
 	//解析模块
 	data := <-l.rc
 	l.wc <- strings.ToUpper(data)
 }
 
-func (l *LogProcess) WriteToInfluxBD() {
-	//写入模块
-	fmt.Println(<-l.wc)
-}
-
 func main() {
-	lp := &LogProcess{
-		rc:          make(chan string),
-		wc:          make(chan string),
-		path:        "/tmp/edward.log",
+	r := &ReadFromFile{
+		path: "/tmp/edward.log",
+	}
+	w := &WriteToInfluxDB{
 		influxBDDsn: "edward.liu",
 	}
+	//使用go fmt 格式化后
+	lp := &LogProcess{
+		rc:    make(chan string),
+		wc:    make(chan string),
+		read:  r,
+		write: w,
+	}
 
-	go lp.ReadFromFile()
+	go lp.read.Read(lp.rc)
 	go lp.Process()
-	go lp.WriteToInfluxBD()
+	go lp.write.Write(lp.wc)
 
 	time.Sleep(1 * time.Second)
 }
