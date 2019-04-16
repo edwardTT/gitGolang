9b5ace6b173cdbca841635fe9dc6fb29d3e9639d
edwardTT <bwxzing@163.com>
2019-04-16 15:00:30 +0800

    读取当前目录文件

--- goroutine/readwriteTest/rwgoroutine.go
+++ goroutine/readwriteTest/rwgoroutine.go
@@ -1,32 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 	"time"
 )
 
 type Reader interface {
-	Read(rc chan string)
+	Read(rc chan []byte)
 }
 
 type Writer interface {
 	Write(wc chan string)
 }
+
 type LogProcess struct {
-	rc    chan string
+	rc    chan []byte
 	wc    chan string
 	read  Reader
 	write Writer
 }
+
 type ReadFromFile struct {
 	path string
 }
 
-func (r *ReadFromFile) Read(rc chan string) {
+func (r *ReadFromFile) Read(rc chan []byte) {
 	// 读取模块
-	line := "message"
-	rc <- line
+	f, err := os.Open(r.path)
+	if err != nil {
+		panic(fmt.Sprintf("open file failed. error %s", err.Error()))
+	}
+	// 从文件末尾开始逐行读取文件内容
+	f.Seek(0, 2)
+
+	rd := bufio.NewReader(f)
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err == io.EOF {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("ReadByte error: %v ", err.Error()))
+		}
+		rc <- line[:len(line)-1]
+
+	}
+
 }
 
 type WriteToInfluxDB struct {
@@ -35,25 +58,31 @@ type WriteToInfluxDB struct {
 
 func (w *WriteToInfluxDB) Write(wc chan string) {
 	//写入模块
-	fmt.Println(<-wc)
+	for v := range wc {
+		fmt.Println(v)
+	}
 
 }
+
+//Process 文件解析方法
 func (l *LogProcess) Process() {
 	//解析模块
-	data := <-l.rc
-	l.wc <- strings.ToUpper(data)
+	for v := range l.rc {
+		l.wc <- strings.ToUpper(string(v))
+	}
+
 }
 
 func main() {
 	r := &ReadFromFile{
-		path: "/tmp/edward.log",
+		path: "./test.log",
 	}
 	w := &WriteToInfluxDB{
 		influxBDDsn: "edward.liu",
 	}
 	//使用go fmt 格式化后
 	lp := &LogProcess{
-		rc:    make(chan string),
+		rc:    make(chan []byte),
 		wc:    make(chan string),
 		read:  r,
 		write: w,
@@ -63,5 +92,5 @@ func main() {
 	go lp.Process()
 	go lp.write.Write(lp.wc)
 
-	time.Sleep(1 * time.Second)
+	time.Sleep(60 * time.Second)
 }
